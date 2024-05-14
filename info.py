3da88f17e66657dc631b7377d0c0dfc24317d54b
COOL_TECH <156781377+Jeronmathew44@users.noreply.github.com>
2024-05-14 16:38:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,6 +11,7 @@ from collections import defaultdict
 from typing import Dict, List, Union
 from pyrogram import Client
 from info import BOT_USERNAME
+from config import BOT_USERNAME, DATABASE_URI, DATABASE_NAME, COLLECTION_NAME, USE_CAPTION_FILTER, MAX_BTN  # Update the import statement
 
 BOT_USERNSME = "GOKU345BOT" # replace here bot username without @
 
