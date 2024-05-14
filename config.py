b563ce7c313b784c63dbb97397d65978cc3c62e7
COOL_TECH <156781377+Jeronmathew44@users.noreply.github.com>
2024-05-14 15:43:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+BOT_USERNAME = "your_bot_username"
+DATABASE_URI = "your_database_uri"
+DATABASE_NAME = "your_database_name"
+COLLECTION_NAME = "your_collection_name"
+USE_CAPTION_FILTER = True  # Or False, depending on your configuration
+MAX_BTN = 10  # Or any other value you need
